dbfb3903f3ba8f40a9c27136bd5f3c9fcdeb172c
Khai Rahman <khairahmanplus@gmail.com>
2018-05-31 09:19:25 +0800

    Exclude authenticated user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::where('id', '!=', auth()->id())->get();
 
         return view('users.index', compact('users'));
     }
